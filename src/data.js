7b3e00f44681f983f93f523448ded180037f7565
lsavoie <twosavoie@gmail.com>
2021-09-10 18:04:19 -0500

    Created a data.js file to hold an array of names that is mapped over to create JSX

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const names = [
+    { personname: "Lisa", id: "0001" },
+    { personname: "Joey", id: "0002" },
+    { personname: "Ann", id: "0003" },
+    { personname: "Sharon", id: "0004" },
+    { personname: "Cristy", id: "0005" },
+    { personname: "Kelli", id: "0006" }
+];
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { names } from "./data.js";
 import NameTag from "./NameTag.js";
 
 import "./styles.css";
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Lisa"/>
-    <NameTag name="Joey"/>
-    <NameTag name="Ann"/>
-    <NameTag name="Sharon"/> 
-    <NameTag name="Cristy"/>
-    <NameTag name="Kelli"/>   
-  </div>
+// previously <NameTag /> was in the App component as:  <NameTag name="Erin" /> and this passed the name to props in NameTag. Now we've created a data.js file to hold an array of names and ids. This callback function now takes in the data from the array and will pass it to the NameTag component through props.
+const renderNameTag = (name) => (
+  <NameTag name={name.personname} key={name.id} />
 );
+
+const App = () => {
+  // mapping to receive the names from the callback function to get the tag JSX for each name
+  const NameTagElements = names.map(renderNameTag);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {/* result of mapping over the callback function */}
+      {NameTagElements}
+    </div>
+  );
+};
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
