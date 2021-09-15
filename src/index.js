bdd47c2871776b7c410c897aef5275dea5512213
lsavoie <twosavoie@gmail.com>
2021-09-14 22:40:25 -0500

    Updated to include state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { names } from "./data.js";
 import NameTag from "./NameTag.js";
 
 import "./styles.css";
 
 // previously <NameTag /> was in the App component as:  <NameTag name="Erin" /> and this passed the name to props in NameTag. Now we've created a data.js file to hold an array of names and ids. This callback function now takes in the data from the array and will pass it to the NameTag component through props.
-const renderNameTag = (name) => (
-  <NameTag name={name.personname} key={name.id} />
-);
+const renderNameTag = (name) => <NameTag name={name} key={name} />
 
-const App = () => {
+
+class App extends Component {
+  state = { names: ["Lisa", "Joey", "Cristy", "Sharon", "Ann", "Kelli"]};
   // mapping to receive the names from the callback function to get the tag JSX for each name
-  const NameTagElements = names.map(renderNameTag);
+  render() {
+  const NameTagElements = this.state.names.map(renderNameTag);
   return (
     <div className="App">
       <h1>Name Tag Generator</h1>
@@ -21,7 +21,7 @@ const App = () => {
     </div>
   );
 };
-
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
